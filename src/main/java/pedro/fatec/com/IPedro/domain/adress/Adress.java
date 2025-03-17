8e29de80104418fe68c23c1e8e9b0316b587e909
Pedro Henrique <pedrohenrique.marineli@gmail.com>
2025-03-17 14:45:05 -0300

    criando a classe de endereço

---
+++ src/main/java/pedro/fatec/com/IPedro/domain/adress/Adress.java
@@ -0,0 +1,15 @@
+package pedro.fatec.com.IPedro.domain.adress;
+
+public class Adress {
+    private Ip ip;
+    private IpBin ipBin;
+    private Mask mask;
+    private MaskBin maskBin;
+    private InMaskBin inMaskbin;
+    private Id id;
+    private IdBin idBin;
+    private IpClass Ipclass;
+    private Broadcast broadcast;
+    private BroadcastBin broadcastBin;
+    private Cidr cidr;
+}
