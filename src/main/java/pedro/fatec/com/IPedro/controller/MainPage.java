a6465192ae450f4ba96c834312445d43ef4580f4
Pedro <pedrohenrique.marineli@gmail.com>
2025-03-17 20:36:15 -0300

    Salvar um ip e um cidr e mostrar na tela

--- src/main/java/pedro/fatec/com/IPedro/controller/MainPage.java
+++ src/main/java/pedro/fatec/com/IPedro/controller/MainPage.java
@@ -3,7 +3,10 @@ package pedro.fatec.com.IPedro.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import pedro.fatec.com.IPedro.domain.adress.Adress;
+import pedro.fatec.com.IPedro.domain.cidr.Cidr;
+import pedro.fatec.com.IPedro.domain.ip.Ip;
 
 @Controller
 public class MainPage {
@@ -14,4 +17,23 @@ public class MainPage {
         model.addAttribute("adress", adress);
         return "index";
     }
+
+    @PostMapping
+    public String saveIp(String ip, String cidr){
+        ip = ip.replace(".", "");
+        Integer ipFirst = Integer.valueOf(ip.substring(0, 3));
+        Integer ipSecond = Integer.valueOf(ip.substring(3, 6));
+        Integer ipThird = Integer.valueOf(ip.substring(6, 9));
+        Integer ipFour = Integer.valueOf(ip.substring(9, 12));
+        Ip ipNew = new Ip(ipFirst, ipSecond, ipThird, ipFour);
+        adress.setIp(ipNew);
+        Cidr cidrNew = new Cidr(cidr);
+        adress.setCidr(cidrNew);
+
+        return "redirect:";
+    }
+
+
+
+
 }
--- src/main/java/pedro/fatec/com/IPedro/domain/cidr/Cidr.java
+++ src/main/java/pedro/fatec/com/IPedro/domain/cidr/Cidr.java
@@ -1,4 +1,24 @@
 package pedro.fatec.com.IPedro.domain.cidr;
 
 public class Cidr {
+    private Integer value;
+
+    public Cidr(String cidr) {
+        value = Integer.valueOf(cidr);
+    }
+
+    public Integer getValue() {
+        return value;
+    }
+
+    public void setValue(Integer value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "Cidr{" +
+                "value=" + value +
+                '}';
+    }
 }
