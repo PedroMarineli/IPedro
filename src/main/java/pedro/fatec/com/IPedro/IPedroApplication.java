e1889c69ca13a40e7b6740488136dc242e05bb22
Pedro Henrique <pedrohenrique.marineli@gmail.com>
2025-03-17 13:41:04 -0300

    spring init

---
+++ src/main/java/pedro/fatec/com/IPedro/IPedroApplication.java
@@ -0,0 +1,13 @@
+package pedro.fatec.com.IPedro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IPedroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IPedroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pedro/fatec/com/IPedro/IPedroApplicationTests.java
@@ -0,0 +1,13 @@
+package pedro.fatec.com.IPedro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IPedroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
